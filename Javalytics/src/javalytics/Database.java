63f15d726dfb41389f7d2e7866e675d18c8376b9
tomsnews <49147550+tomsnews@users.noreply.github.com>
2019-08-13 09:53:20 +0200

    SQL to Array added

--- Javalytics/src/javalytics/Database.java
+++ Javalytics/src/javalytics/Database.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class Database {
 	
@@ -122,5 +123,70 @@ public class Database {
 		return rs;
 		
 	}	
+	
+	
+	/**
+	 * @author: Tom Papatolis
+	 * 
+	 * A functions that returns the first column of a ResultSet as a String Array
+	 * 
+	 * @param query Your SQL Command
+	 * @return a String Array containing values from the first column
+	 */
+	public static String[] getDataInArray(String query) {
+		
+		try {
+			
+			// Open Connection
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			String server 	 = "localhost";  
+			int port		 = 3306; 			
+			String database  = "javalytics";
+			String user		 = "root";
+			String passwd 	 = "";
+			String con_str   = "jdbc:mysql://" + server + ":" + port + "/" + database + "?serverTimezone=UTC";
+			Connection con 	 = DriverManager.getConnection(con_str, user, passwd);			
+			
+			// Execute Query
+			Statement stmt = con.createStatement();						
+			ResultSet rs   = stmt.executeQuery(query);
+	
+			// Put Results into ArrayList
+			ArrayList<String> column1 = new ArrayList<String>();
+			//ArrayList<String> column2 = new ArrayList<String>();
+
+			while (rs.next()) { 
+				column1.add(rs.getString(1));
+				//column2.add(rs.getString(2));
+			}
+
+			// Finally turn the array lists into arrays
+			String[] column1Arr = new String[column1.size()];
+			column1Arr = column1.toArray(column1Arr);
+
+			/*
+			String[] column2Arr = new String[column2.size()];
+			column2Arr = column2.toArray(column2Arr);
+			*/			
+	
+			con.close();
+			
+			System.out.println("cool");
+			
+			return column1Arr;			
+			
+		} catch (ClassNotFoundException e) {
+			System.err.println("JDBC-Driver for MySQL was not found.");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.err.println("SQL command failed.");
+			e.printStackTrace();
+		}
+		
+		return null;
+		
+	}	
+	
+	
 
 }
